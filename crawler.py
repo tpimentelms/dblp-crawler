4a899adc93d7a7e08584329ae8087d985b7ba36c
Tiago Pimentel <tiagopms@gmail.com>
2016-10-07 15:54:50 -0300

    Saving results

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import argparse
 import mechanize
 import re
 from bs4 import BeautifulSoup, SoupStrainer
-
+import pickle
 
 def parse_args():
     '''
@@ -10,7 +10,7 @@ def parse_args():
     '''
     parser = argparse.ArgumentParser(description="Get node embeddings.")
 
-    parser.add_argument('--output', default='dblp-data.csv',
+    parser.add_argument('--output', default='dblp-data.pckl',
                         help='Output file.')
 
     parser.add_argument('--verbose', dest='verbose', action='store_true',
@@ -21,14 +21,14 @@ def parse_args():
 
 authors = set()
 coauthors = {}
-ambiguous = []
+ambiguous = {}
 
 
 # http://dblp.uni-trier.de/pers/hd/z/Zhang:Shao_Jie
 # http://dblp.uni-trier.de/pers/hd/g/Gao_0001:Wen?q=Wen%24+Gao%24+0001%24
 # http://dblp.uni-trier.de/pers/hd/p/Poor:H=_Vincent?q=H.%24+Vincent%24+Poor%24
 def get_author_id_from_url(url):
-    regex = 'http:\/\/dblp\.uni-trier\.de\/pers\/hd\/\w\/([^\?\n]+)(\?q=)?'
+    regex = 'http:\/\/dblp\.uni-trier\.de\/pers\/hd\/[\w=]\/([^\?\n]+)(\?q=)?'
     m = re.match(regex, url)
     if not m:
         raise Exception("Error with url %s being parsed as author" % url)
@@ -69,35 +69,38 @@ def get_author_id_from_url(url):
 # </div>
 
 def parse_author_page(page, name):
+    global coauthors, authors
     author_id = get_author_id_from_url(page.geturl())
     if author_id in authors:
         return
     authors.add(author_id)
     coauthors[author_id] = []
-    
+
     print '\tGetting author: %s' % (author_id)
 
-    # strainer = SoupStrainer('div', attrs={'id': 'publ-section'})
-    # strainer = SoupStrainer('ul', attrs={'class': 'publ-list'})
     strainer = SoupStrainer('div', attrs={'class': 'data', 'itemprop': 'headline'})
     soup = BeautifulSoup(page, 'lxml', parse_only=strainer)
 
-    # for div in soup.find_all('div', {'class': 'data'}):
     for tag in soup.find_all('span', {'itemprop': 'author'}):
         if tag.a: # Author is not self
             coauthor_url = tag.a['href']
             coauthor_id = get_author_id_from_url(coauthor_url)
 
-            coauthors[author] += [coauthor_id]
+            coauthors[author_id] += [coauthor_id]
 
-
+    return author_id
 
 
 def parse_disambiguation_page(br, name):
+    global ambiguous
     print '\tDisambiguating: %s' % (name)
+    ambiguous[name] = []
 
     for link in br.links(url_regex="pers\/hd\/"):
-        crawl_page(br, link)
+        author_id = crawl_page(br, link)
+        ambiguous[name] += [author_id]
+
+    return None
 
 
 def crawl_page(br, link):
@@ -108,11 +111,12 @@ def crawl_page(br, link):
 
     regex = 'dblp: Author search for '
     if re.match(regex, page_title):
-        parse_disambiguation_page(br, name)
+        result = parse_disambiguation_page(br, name)
     else:
-        parse_author_page(page, name)
+        result = parse_author_page(page, name)
 
     br.back()
+    return result
 
 
 def crawl_prolific_page(index):
@@ -126,12 +130,27 @@ def crawl_prolific_page(index):
         crawl_page(br, link)
 
 
+def save_data(args):
+    global coauthors, authors, ambiguous
+    data = (coauthors, authors, ambiguous)
+    with open(args.output, 'wb') as output:
+        pickle.dump(data, output)
+
+
 def main(args):
     if args.verbose:
         print 'Verbose'
 
     for index in range(1, 12):
         crawl_prolific_page(index)
+    # try:
+    #     for index in range(1, 12):
+    #         crawl_prolific_page(index)
+    # except Exception as e:
+    #     print 'Error ocurred! Saving temp data'
+    #     print e
+
+    save_data(args)
 
 
 if __name__ == '__main__':
